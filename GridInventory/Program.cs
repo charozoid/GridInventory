3ae51a3a3d91707417eee2383ab39d63730b4fa0
charozoid <directfromquebec@gmail.com>
2023-12-03 11:25:19 -0500

    Added drag and drop, item size detection, color feedback if space is available

--- GridInventory/Program.cs
+++ GridInventory/Program.cs
@@ -2,36 +2,39 @@
 using SFML.System;
 using SFML.Graphics;
 using SFML.Window;
-
 class GridInventory
 {
-    public static Clock clock = new Clock();
+    public static Inventory inv = new Inventory();
     public static void Main(string[] args)
     {
         Graphics.window.SetVerticalSyncEnabled(true);
         Graphics.window.Closed += (sender, args) => Graphics.window.Close();
-        Inventory inv = new Inventory();
-        Item ak47 = new Item();
-        ak47.IntRect = new IntRect(0, 0, 192, 64);
-        ak47.size = new Vector2i(6, 2);
-        inv.AddItem(ak47);
+        Graphics.window.MouseButtonPressed += inv.ButtonPressed;
+        Graphics.window.MouseButtonReleased += inv.ButtonReleased;
+        Graphics.window.KeyPressed += EventHandler;
+
         while (Graphics.window.IsOpen)
         {
             Graphics.window.Clear(Color.Black);
+            
             Graphics.DrawGrid();
+            inv.Think();
             Graphics.DrawItems();
-            KeyPressed(inv);
+            
             Graphics.window.DispatchEvents();
             Graphics.window.Display();
         }
     }
-    public static void KeyPressed(Inventory inv)
+    public static void EventHandler(object sender, KeyEventArgs e)
     {
-        Time elapsedTime = clock.ElapsedTime;
-        if (Mouse.IsButtonPressed(Mouse.Button.Left) && elapsedTime.AsMilliseconds() >= 200)
+        switch (e.Code)
         {
-            inv.LeftClick(Graphics.MousePos());
-            clock.Restart();
+            case (Keyboard.Key.Space):
+                Item ak47 = new Item();
+                ak47.IntRect = new IntRect(0, 0, 192, 64);
+                ak47.size = new Vector2i(6, 2);
+                inv.AddItem(ak47);
+                break;
         }
     }
 }
@@ -40,10 +43,10 @@ class Graphics
     public const int WIDTH = 960;
     public const int HEIGHT = 576;
     public static VideoMode mode = new VideoMode(WIDTH, HEIGHT);
-    public static RenderWindow window = new RenderWindow(mode, "Isometric");
+    public static RenderWindow window = new RenderWindow(mode, "Inventory");
     public static Texture gunsTexture = new Texture("../../Assets/guns.png");
     public static List<Item> itemList = new List<Item>();
-
+    
     public static void DrawGrid()
     {
         Sprite sprite = new Sprite(gunsTexture);
@@ -56,10 +59,6 @@ class Graphics
                 window.Draw(sprite);
             }
         }
-    }
-    public static void CreateItem()
-    {
-
     }
     public static void DrawItems()
     {
@@ -83,9 +82,10 @@ class Graphics
 class Item
 {
     public Vector2i inventoryPos { get; set; }
-    private Sprite sprite;
+    public Sprite sprite {get; set;}
     public IntRect intRect;
     public Vector2i size { get; set; }
+    public Vector2i oldPos { get; set; }
     public IntRect IntRect {  
         get { return intRect; }
         set {
@@ -113,13 +113,15 @@ class Item
 
 class Inventory
 {
+    public Clock clock = new Clock();
     public Item?[,] itemGrid = new Item[30, 18];
-    public Item selectedItem;
+    public Item? selectedItem;
     private bool hasItemSelected = false;
+
     public void MoveItem(Item item, Vector2i gridCoords)
     {
-        int itemX = item.InventoryPos.X;
-        int itemY = item.InventoryPos.Y;
+        int itemX = item.oldPos.X;
+        int itemY = item.oldPos.Y;
         for (int i = itemX; i < item.size.X + itemX; i++)
         {
             for (int j = itemY; j < item.size.Y + itemY; j++)
@@ -140,9 +142,28 @@ class Inventory
     }
     public void AddItem(Item item)
     {
-        item.InventoryPos = new Vector2i(2, 2);
         int itemX = item.InventoryPos.X;
         int itemY = item.InventoryPos.Y;
+
+        int startingIndex = 0;
+        int count = 0;
+        while (count < item.size.X)
+        {
+            for (int i = startingIndex; i < item.size.X + itemX + startingIndex; i++)
+            {
+                if (itemGrid[i, 0] != null)
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            startingIndex++;
+        }
+        item.InventoryPos = new Vector2i(startingIndex - 1, 0);
+
         for (int i = itemX; i < item.size.X + itemX; i++)
         {
             for (int j = itemY; j < item.size.Y + itemY; j++)
@@ -174,6 +195,10 @@ class Inventory
         }           
         return itemGrid[gridCoords.X, gridCoords.Y];
     }
+    public bool IsSquareEmpty(Vector2i gridCoords)
+    {
+        return itemGrid[gridCoords.X, gridCoords.Y] == null;
+    }
     public void LeftClick(Vector2i mousePos)
     {
         if (selectedItem != null)
@@ -187,5 +212,81 @@ class Inventory
             selectedItem = GridToItem(mousePos);
         }
 
+    }
+    public void ButtonReleased(object sender, MouseButtonEventArgs e)
+    {
+        Vector2i mousePos = Graphics.MousePos();
+        switch (e.Button)
+        {
+            case (Mouse.Button.Left):
+                if (selectedItem != null)
+                {
+                    bool areSquaresAvailable = true;
+                    for (int i = mousePos.X; i < mousePos.X + selectedItem.size.X; i++)
+                    {
+                        for (int j = mousePos.Y; j < mousePos.Y + selectedItem.size.Y; j++)
+                        {
+                            if (itemGrid[i, j] != null && itemGrid[i, j] != selectedItem)
+                            {
+                                areSquaresAvailable = false;
+                            }
+                        }
+                    }
+                    if (areSquaresAvailable)
+                    {
+                        MoveItem(selectedItem, mousePos);
+                        selectedItem = null;
+                        hasItemSelected = false;
+                    }
+                    else
+                    {
+                        MoveItem(selectedItem, selectedItem.oldPos);
+                        selectedItem = null;
+                        hasItemSelected = false;
+                    }
+                }
+            break;
+        }
+    }
+
+    public void ButtonPressed(object sender, MouseButtonEventArgs e)
+    {
+        Vector2i mousePos = Graphics.MousePos();
+        switch (e.Button)
+        {
+            case (Mouse.Button.Left):
+                selectedItem = GridToItem(mousePos);
+                if (selectedItem != null)
+                    selectedItem.oldPos = selectedItem.InventoryPos;
+                break;
+        }
+
+    }
+    public void Think()
+    {
+        if (selectedItem != null)
+        {
+            Vector2i mousePos = Graphics.MousePos();
+            selectedItem.InventoryPos = mousePos;
+            RectangleShape rectangle = new RectangleShape((Vector2f)(selectedItem.size * 32));
+            rectangle.Position = selectedItem.sprite.Position;
+            rectangle.FillColor = new Color(0, 225, 0, 175);
+            for (int i = mousePos.X; i < mousePos.X + selectedItem.size.X; i++)
+            {
+                for (int j = mousePos.Y; j < mousePos.Y + selectedItem.size.Y; j++)
+                {
+                    if (i < 30 && i > 0 && j < 18 && j > 0)
+                    {
+                        if (itemGrid[i, j] != null && itemGrid[i, j] != selectedItem)
+                        {
+                            rectangle.FillColor = new Color(225, 0, 0, 175);
+                        }
+                    }
+
+                }
+            }
+
+            Graphics.window.Draw(rectangle);
+        }
     }
 }
